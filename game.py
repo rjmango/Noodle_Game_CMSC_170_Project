65679bfed630f902e05009cfe8829b9f1ec894e9
rjmango <you@example.comgit config --global user.email you@example.com>
2025-03-18 14:22:46 +0800

    Initial commit | two player screen implemented

---
+++ game.py
@@ -0,0 +1,416 @@
+#Julius M. Manigo Jr.
+#CMSC 12 T14L Laboratory
+#Module for the game functions
+
+import random
+import os
+import shutil
+
+COL_WIDTH =  int(shutil.get_terminal_size().columns // 2) - 3
+counters = {}																	# Counter:[noodle preference,patience]
+trays = {'A':[0,0],'B':[0,0],'C':[0,0], 'D':[0,0], 'E':[0,0], 'F':[0,0]}		# Tray:[Occupied,noodle doneness]
+aiCounters = {}
+aiTrays = {'A':[0,0],'B':[0,0],'C':[0,0], 'D':[0,0], 'E':[0,0], 'F':[0,0]}
+
+def Game_Menu():											#Print game Menu
+	print("1 - PLACE noodle bowl in the tray")
+	print("2 - SERVE noodle bowl")
+	print("3 - DISCARD noodle bowl in the tray")
+	print("4 - WAIT and skip a turn")
+
+def counter_Preparation(diffi,counters):					#Prepare number of counters based on difficulty and returns the score multiplier
+	letters = "ABCDEF"
+	
+	if diffi == "EASY":
+		x = 3
+		for k in range(0,x):
+			counters[letters[k]] = [0,""]
+		return 1
+	elif diffi == "MEDIUM":
+		x = 4
+		for k in range(0,x):
+			counters[letters[k]] = [0,""]
+		return 2
+	elif diffi == "HARD":
+		x = 5
+		for k in range(0,x):
+			counters[letters[k]] = [0,""]
+		return 3
+
+def Patience(x):											#Returns a number of patience for each order depending on the preference
+	if x == 1:
+		return random.randint(5,8) #dipped
+	if x == 2:
+		return random.randint(6,9) #extra firm
+	if x == 3:
+		return random.randint(7,10) #firm
+	if x == 4:
+		return random.randint(8,11) #regular
+	if x == 5:
+		return random.randint(9,12) #soft
+	if x == 6:
+		return random.randint(10,13) #extra soft
+	if x == 7:
+		return random.randint(10,14) #overcooked
+	else:
+		return "" 		
+
+def counter_System(counters,vals):							#System for the counters
+	list_counter=[k for k in counters.keys()]
+	for c in list_counter:									#Decrease patience of each customer counter is taken
+		if counters[c][0] != 0:
+			counters[c][1] -= 1
+
+		if counters[c][0] == 0 and vals["count"] != vals["total"]:		#Adds new customer when a counter is empty and there are still customers yet to serve
+			counters[c][0] = random.randint(0,7)
+			counters[c][1] = Patience(counters[c][0])
+			if counters[c][0] != 0:
+				vals["count"] += 1
+
+def unsuccessful_Order(counters,streak):					#Function for updating counters when customers patience reach zero
+	x = 0
+	list_counter=[k for k in counters.keys()]
+	for c in list_counter:									#Resets the counter into default when order is unsuccessful
+		if counters[c][0] != 0 and counters[c][1] == 0:
+			counters[c] = [0,""]
+			streak["current"] = 0
+			x = 1
+			print("WARNING! Customer at "+c+" walked away!")
+	if x == 1:
+		print("")
+
+def print_Subcounter(subcounter):								#Prints the counter
+	Noodle_Preference={0:"Empty",
+					   1:"Dipped",
+					   2:"Extra_Firm",
+					   3:"Firm",
+					   4:"Regular",
+					   5:"Soft",
+					   6:"Extra_Soft",
+					   7:"Overcooked"}
+	
+	if subcounter == '':
+		return
+
+	left = ""
+	right = ""
+	for counter in subcounter:								#Prints customer preference or when empty
+		v = counters[counter]
+		left += f"{Noodle_Preference[v[0]]:^25}"
+		right += f"{Noodle_Preference[v[0]]:^25}"
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+	
+	out = ""
+	for counter in subcounter:								#Visual design for each counter
+		out += f"{'-----------':^25}"
+	print(out.ljust(COL_WIDTH) + ' | ' + out.ljust(COL_WIDTH))
+
+	left = ""
+	right = ""
+	for counter in subcounter:								#Print counter name
+		left += f"{'Counter '+counter:^25}"
+		right += f"{'Counter '+counter:^25}"
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+
+	left = ""
+	right = ""
+	for counter in subcounter:								#Prints patience when there is a customer
+		v = counters[counter]
+		if v[0] == 0:									
+			left += f"{'':^25}"
+			right += f"{'':^25}"
+		else:
+			left += f"{'Patience: '+str(v[1]):^25}"
+			right += f"{'Patience: '+str(v[1]):^25}"
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))	
+	print_NextLine()
+		
+def input_Tray(trays):										#Function for inputing trays
+	while True:
+		Tray_input = input("Choose TRAY to PLACE: ")
+		if Tray_input.upper() in trays:						#Checks whether tray is occupied
+			if trays[Tray_input.upper()][0] == 1:
+				print("Tray is already occupied")
+			else:
+				trays[Tray_input.upper()][0] = 1 			#Update status from 0/empty to 1/occupied
+				break
+		else:
+			print("Invalid Input!")
+
+def discard_Tray(trays):									#Function for discarding trays
+	while True: 
+		Tray_input = input("Choose TRAY to DISCARD: ")
+		if Tray_input.upper() in trays:						#Checks whether tray is empty
+			if trays[Tray_input.upper()][0] == 0:
+				print("Tray is already empty")
+			else:
+				trays[Tray_input.upper()] = [0,0]			#Resets tray into default status
+				break
+		else:
+			print("Invalid Input!")
+
+def increment_Tray(trays):									#Increments each tray when occupied
+	for v in trays.values():
+		if v[0] == 1:										#Checks if tray is occupied
+			if v[1] == 7:									#Checks if noodle is overcooked
+				pass
+			else:
+				v[1] += 1
+
+def print_Subtray(subtray):										#Prints each tray
+	Noodle_Doneness={"0":"Empty",
+				  	 "1":"Dipped",
+					 "2":"Extra Firm",
+					 "3":"Firm",
+					 "4":"Regular",
+					 "5":"Soft",
+					 "6":"Extra Soft",
+					 "7":"Overcooked"
+					}
+
+	left = ""
+	right = ""
+	for tray in subtray:								#Visual design for each counter
+		Doneness = Noodle_Doneness[str(trays[tray][1])]
+		left += f"{Doneness:^25}"
+		right += f"{Doneness:^25}"
+	
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+
+	out = ""
+	for tray in subtray:								
+		out += f"{'-----------':^25}"
+	print(out.ljust(COL_WIDTH) + ' | ' + out.ljust(COL_WIDTH))
+
+	left = ""
+	right = ""
+	for tray in subtray:								
+		left += f"{'Slot: ' +tray:^25}"
+		right +=  f"{'Slot: ' +tray:^25}"
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+	print_NextLine()
+
+def counter_Empty(counters):								#Checks whether counter is emtpy
+	list_counter=[k for k in counters.keys()]
+	for c in list_counter:
+		if counters[c][0] != 0:
+			return False
+	return True	
+
+def tray_Full(trays):										#Checks whether tray is full
+	list_tray=[k for k in trays.keys()]
+	for t in list_tray:
+		if trays[t][0] != 1:
+			return False
+	return True 
+
+def tray_Empty(trays):										#Checks whether tray is empty
+	list_tray=[k for k in trays.keys()]
+	for t in list_tray:
+		if trays[t][0] != 0:
+			return False
+	return True 
+
+def empty_Tray(trays):										#Empties all tray slots
+	for k in trays.keys():
+		trays[k]=[0,0] 										#Resets each tray slot into its default values
+
+def main_Action(trays,counters,vals,quota,streak,diffi,name):	#Function for choosing main action in game
+	while True:
+		action = input("\nChoose action: ")
+		if action == "1":
+			if tray_Full(trays) == False:					#Checks if tray is not full
+				input_Tray(trays)							#Inputs a tray in slot
+				break
+			else:
+				print("Tray is full")
+		elif action == "2":
+			if counter_Empty(counters) == True:				#Checks if all counters are empty
+				print("There are no customers to serve!")
+			elif tray_Empty(trays) == True:					#Checks if all trays are empty
+				print("All of the tray slots are empty! You need to place a noodle bowl first.")
+			else:
+				serve(trays,counters,vals,quota,streak)		#Serves a selected tray slot to a selected counter
+				break
+		elif action == "3":
+			if tray_Empty(trays) == False:					#Checks if all trays are not empty
+				discard_Tray(trays)							#Empties a tray in slot
+				break
+			else:
+				print("Tray is already empty")
+		elif action == "4":
+			break
+		else:
+			print("Invalid Action!")
+
+def Quota(diffi,vals):										#Returns the quota depending on the difficulty
+	if diffi == 'EASY':
+		return int(vals["total"]*0.5)
+	if diffi == 'MEDIUM':
+		return int(vals["total"]*0.75)
+	else:
+		return vals["total"]
+
+def serve(trays,counters,vals,quota,streak):				#Function for serving
+	while True:												#Tray Entry
+		pick_tray = input("Enter TRAY slot: ")
+		if pick_tray.upper() in trays:
+			if trays[pick_tray.upper()][0] == 0:
+				print ("Tray slot is empty")
+			else:
+				break
+		else: print("Invalid Input!")
+
+	while True:												#Counter Entry
+		pick_counter = input("Enter COUNTER to SERVE: ")
+		if pick_counter.upper() in counters:
+			if counters[pick_counter.upper()][0] == 0:
+				print("Counter is empty")
+			else: 
+				break
+		else: print("Invalid Input!")
+
+	if trays[pick_tray.upper()][1] == counters[pick_counter.upper()][0]:	#Checks if customer preference is equal to the doneness of noodle served
+		print("\nOrder Success!")
+		vals["served"] += 1 														#Adds one to successful order count
+		vals["score"] += int(35 + 0.15*streak["current"])									#Adds score rounded down to nearest integer. Score dedpends on the streak
+		streak["current"] += 1
+		if streak["current"] > streak["longest"]:
+			streak["longest"] = streak["current"]											#Updates the maximum streak
+	else:
+		print("\nOrder Unsuccesful")
+		if vals["score"] > 10:													#Score cannot be negative
+			vals["score"] -= 10													#Subtracts score when order unsuccesful
+		streak["current"] = 0 														#Resets streak
+
+	trays[pick_tray.upper()] = [0,0]										#Resets the default value for the selected tray
+	counters[pick_counter.upper()] = [0,""]									#Resets the defaul value for the selected counter
+
+def game_Over(name,diffi,total,vals,multiplier,streak):						#Prints the journey of the customer when game over
+	os.system("cls")
+	print("Summary of", name+"'s journey","\n")
+	print("="*45)
+	print("Difficulty:", diffi)
+	print("Total number of customers:", total["count"])
+	print("Total number of successful orders:", total["served"])
+	print("Longest Streak:",streak["longest"])
+	print("Total number of rounds:", vals["rounds"])
+	print("Final Score:",vals["score"]*multiplier)
+	print("="*45 + "\n")
+	placeholder = input("Press Enter to continue.")
+
+def print_Header(string):
+	print_NextLine()
+	out = string.center(25)
+	print(out.ljust(COL_WIDTH) + ' | ' + out.ljust(COL_WIDTH))
+	print_NextLine()
+
+def print_NextLine():
+	print("".ljust(COL_WIDTH) + ' | ' + "".ljust(COL_WIDTH))
+
+def print_Border():
+	print("="*(COL_WIDTH+1)+"|"+"="*(COL_WIDTH+4))
+
+def print_Display(name,diffi,vals,quota,streak):							#Prints the Display
+
+	# Prints the names of both player and AI
+	print(name.ljust(COL_WIDTH) + ' | ' + "AI".ljust(COL_WIDTH))
+	
+	# Prints the score and streak of both player and AI
+	left = (f'{("Score: "+str(vals["score"])).ljust((COL_WIDTH//2))}'
+		 	f'{("Streak: "+str(streak["current"])).rjust(COL_WIDTH//2)}')
+	right = (f'{("Score: "+str(vals["score"])).ljust((COL_WIDTH//2))}'
+		  	 f'{("Streak: "+str(streak["current"])).rjust(COL_WIDTH//2+3)}')
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+
+	# Prints the customers to serve and successful orders of both player and AI
+	left = (f'{("Customers to serve: "+str(vals["total"]-vals["count"])).ljust((COL_WIDTH//2))}'
+		    f'{("Successful Orders: "+str(vals["served"])).rjust(COL_WIDTH//2)}')
+	right = (f'{("Customers to serve: "+str(vals["total"]-vals["count"])).ljust((COL_WIDTH//2))}'
+		    f'{("Successful Orders: "+str(vals["served"])).rjust(COL_WIDTH//2+3)}')
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+	
+	# Prints the total customers to serve and the quota for the round
+	left = (f'{("Customers to serve: "+str(vals["total"]-vals["count"])).ljust((COL_WIDTH//2-3))}'
+		    f'{("Quota for the round: "+str(quota)).rjust(COL_WIDTH//2+3)}')
+	right = (f'{("Customers to serve: "+str(vals["total"]-vals["count"])).ljust((COL_WIDTH//2))}'
+		    f'{("Quota for the round: "+str(quota)).rjust(COL_WIDTH//2+3)}')
+	print(left.ljust(COL_WIDTH) + ' | ' + right.ljust(COL_WIDTH))
+
+	print_Border()															# Prints the border
+	print_Header("[Trays]")													# Prints the [Trays] header
+	print_Subtray("ABC")													# Prints the trays ABC
+	print_Subtray("DEF")													# Prints the trays DEF	
+	print_Header("[Counters]")												# Prints the [Trays] header
+	print_Subcounter("ABC")													# Prints the counters
+	remainingCounters = {'EASY': '', 'MEDIUM': 'D', 'HARD': 'DE'}[diffi]	# Checks which counters to print depending on the difficulty
+	print_Subcounter(remainingCounters)										# Prints the remaining counter
+	print_Border()
+
+def game_Function(name,diffi,trays,counters):								#Function for the game
+	# Player records
+	playerStreak = {"current": 0, "longest": 0}
+	playerVals = {"count": 0, "total": 10, "rounds": 1, "served": 0, "score": 0}
+	playerTotal = {"count": 0, "served": 0}
+	
+	# AI records
+	aiStreak = {"current": 0, "longest": 0} 
+	aiVals = {"count": 0, "total": 10, "rounds": 1, "served": 0, "score": 0}
+	aiTotal = {"count": 0, "served": 0}
+
+	# ingame variables
+	multiplier = counter_Preparation(diffi,counters)						#Stores the final score multiplier
+	proceed = 1 															#Indicator whether or whether you can proceed next round.
+	while True:
+
+		if proceed == 0:													#Ends the game when proceed is 0
+			game_Over(name,diffi,playerTotal,playerVals,multiplier,playerStreak)				#Prints the players summary of journey
+			break
+
+		quota = Quota(diffi,playerVals)											#Stores the quota
+		os.system("cls")
+
+		while True:
+			if playerVals["count"] == playerVals["total"] and counter_Empty(counters) == True:			#If max count of customers, the round ends
+				if playerVals["served"] < quota:														#Checks if customers served does reaches the quota
+					os.system("cls")
+					print("Level Failed!\nYou failed to reach the required number of successful orders.")
+					proceed = 0													#Sets the player elligible for next round
+					empty_Tray(trays)											#Empties the tray
+					playerTotal["count"] += playerVals["count"]					#Records the total count of customers
+					playerTotal["served"] += playerVals["served"]				#Records the total rounds
+					placeholder = input("Press ENTER to continue.")
+					break
+
+				else:
+					os.system("cls")
+					print("Level Completed!\nPreparing the next level.")
+					placeholder = input("Press ENTER to continue.")
+					empty_Tray(trays)
+					playerVals = {	"count": 0, 								#updates the Count, Total, Rounds, Served, and Score
+				   					"total": playerVals["total"]+5,
+									"rounds": playerVals["rounds"]+1,
+									"served": 0,
+									"score": playerVals["score"]
+								}
+					playerTotal["count"] += playerVals["count"]					#Records the total count of customers
+					playerTotal["served"] += playerVals["served"]				#Records the total rounds
+					break
+
+			print_Display(name,diffi,playerVals,quota,playerStreak)				#Prints the display
+			
+			Game_Menu()															#Prints the game menu
+
+			main_Action(trays,counters,playerVals,quota,playerStreak,diffi,name)			#Calls the function for the main function
+
+			increment_Tray(trays)															#Increments the tray
+
+			counter_System(counters,playerVals)												#Carries out the function for the system of counters
+
+			input("\nPress ENTER to continue.")
+
+			os.system("cls")
+
+			unsuccessful_Order(counters,playerStreak)								#Decrements each patience of the customer
+	final_score = playerVals["score"]*multiplier									#Multiplies score to the multiplier and stores in a variable
+	return [final_score,name,playerVals["rounds"],diffi]							#returns the following in order: final score, name, rounds, difficulty
---
+++ main.py
@@ -0,0 +1,132 @@
+#Julius M. Manigo Jr.
+#CMSC 12 T14L Laboratory
+#Project
+#Python file for the main process
+
+from game import *							#Imports everything from the game.py
+import os 									#Imports os library
+os.system("cls")
+
+def Difficulty():							#Asks and sets the difficulty
+	os.system("cls")
+	while True:
+		print("="*42)
+		print("\n"+f"{'[CHOOSE DIFFICULTY]':^42}"+"\n")
+		print(f"{'[1] EASY ':^14}"f"{'[2] MEDIUM':^14}"f"{'[3] HARD ':^14}")
+		print(f"{'50% QUOTA':^14}"f"{'75% QUOTA':^14}"f"{'100% QUOTA':^14}")
+		print(f"{'3 COUNTERS':^14}"f"{'4 COUNTERS':^14}"f"{'5 COUNTERS':^14}")
+		print("\n"+"="*42)
+	
+		diff = input("\nEnter choice here: ")
+
+		if diff == "1":
+			return "EASY"
+		elif diff == "2":
+			return "MEDIUM"
+		elif diff == "3":
+			return "HARD"
+		else: 
+			os.system('cls')
+			print("Invalid Input!")
+
+def Main_Menu():							#Prints the main menu
+	print("Welcome to the Noodle Shop Game!")
+	print("[1] PLAY")
+	print("[2] VIEW HIGHSCORE")
+	print("[3] HOW TO PLAY")
+	print("[4] QUIT")
+
+def save_Score(record):						#Saves the score to an external txt file
+	f = open("records.txt", "a")			#Opens the txt file for the scores
+
+	f.write(str(record[0])+","+record[1]+","+str(record[2])+","+record[3]+"\n")		#Writes the final score, name, rounds and dificulty in one line respectively
+	#writes the score, name, rounds, and difficulty in a file
+
+	f.close()								#Closes the txt file for the scores
+
+def load_Scores():							#Unloads the score from the txt file
+	f = open('records.txt','r')				#Opens the txt file for the score
+	new_records = []						#Container for the recorded redords from the previous games.
+	sorting = []							#Container for the sorted scores
+	for line in f:
+		token = line[:-1].split(',')		#Splits the records
+		list1 = [int(token[0]),token[1],token[2],token[3]]		#List for score,name,rounds, and difficulty
+		new_records.append(list1)			#Adds the splitted records to the new records
+		sorting.append(int(token[0]))		#Adds the score to the sorting list.
+	sorting.sort(reverse=True)				#Sorts each score in descending order
+	f.close()								#Closes the txt file for the scores
+	return [new_records,sorting]
+
+def print_Loaded(values):					#Prints the unpacked records
+	if len(values[0]) == 0:					#Checks whether there are no existing records
+		print("There are no existing records.")
+	else:									#Prints the records
+		print("%-15s %-15s %-15s %-15s %s"%("Rank","Score","Name","Rounds","Difficulty"),"\n")
+		rank = 1
+		for each in values[1]:
+			for element in values[0]:
+				if each == element[0]:
+					print("%-15s %-15s %-15s %-15s %s"%(rank,element[0],element[1],element[2],element[3]))
+					rank+=1
+					values[0].remove(element)
+
+def name_Input():							#Asks for the name and returns it as a value
+	os.system("cls")
+	while True:
+		name = input("Enter your name? (Must be 1 to 10 characters)\n")		#System will only accepts names less than 11 characters
+		if len(name)>11:
+			print("Your name exceeded 10 characters.")
+		elif len(name)<1:
+			print("Your name must be at least 1 character.")
+		else:
+			return name
+
+def guidelines():								#Prints the guidelines for the game
+	line_groups=[6,10,9,10,12,5,7,14]			#Line groups to print at a time
+	f = open('tutorial.txt','r')				#Opens the txt file for the guidelines
+	for x in range(0,8):
+		os.system("cls")
+		for y in range(0,line_groups[x]):
+			print(f.readline())
+		if x != 7:
+			placeholder = input("Press ENTER to Proceed")
+	f.close()									#Closes the txt fiel for the guidelines
+
+#mainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainmainloop
+def main():
+	while True:
+		Main_Menu()
+		main_action = input("Enter choice: ")		#Asks for the main action of the game
+
+		if main_action == "1":
+			name = name_Input()						#Stores the name
+			diff = Difficulty()						#Stores the difficulty chosen
+			print("\n"+"You picked", diff)
+			placeholder = input("Press Enter to continue.")
+
+			record = game_Function(name,diff,trays,counters)		#Carries out the game function and records the score, name, rounds and difficulty
+			save_Score(record)						#Saves the records to the respective txt file
+
+		elif main_action == "2":
+			os.system("cls")
+			print_Loaded(load_Scores())				#Prints the records
+			placeholder = input("\nPress Enter to go back")
+			os.system("cls")
+
+		elif main_action == "3":
+			os.system("cls")
+			guidelines()							#Prints the guidelines
+			placeholder = input("\nPress Enter to quit tutorial")
+			os.system("cls")
+
+		elif main_action == "4":
+			print("Thank you for playing!")			#Exits the code.
+			break
+		
+		else:
+			os.system("cls")
+			print("Invalid input.")
+			print("")
+			
+if __name__ == "__main__":
+	main()
